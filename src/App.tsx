bc96444d08eccea6e24a6447b6baf8e883452b9d
jihed <bousnina.jihed@gmail.com>
2024-11-10 16:35:12 +0100

    created types, fetch & process function successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { fetchNeoData } from './api/nasaApi';
+import { processNeoData } from './utils/dataProcessor';
 
 function App() {
+  useEffect(() => {
+    const getNeoData = async () => {
+      try {
+        const data = await fetchNeoData()
+        const processedData = processNeoData(data)
+        console.log('processedData', processedData)
+      } catch (err: any) {
+        console.error(err.message)
+      } 
+    }
+
+    getNeoData()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
